348fc7f2f9f139b1627f2f436b7b45c4b75aa35b
chetanakkatangerhal <78865519+chetanakkatangerhal@users.noreply.github.com>
2023-05-03 15:12:38 +0530

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_demo/counter_bloc.dart';
+import 'package:flutter_bloc_demo/counter_event.dart';
+import 'package:flutter_bloc_demo/counter_state.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter BLoC Demo',
+      home: BlocProvider(
+        create: (context) => CounterBloc(),
+        child: CounterPage(),
+      ),
+    );
+  }
+}
+
+class CounterPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final CounterBloc _counterBloc = BlocProvider.of<CounterBloc>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter BLoC Demo'),
+      ),
+      body: BlocBuilder<CounterBloc, CounterState>(
+        builder: (context, state) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Count:',
+                ),
+                Text(
+                  '${state.count}',
+                  style: Theme.of(context).textTheme.headline4,
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+      
